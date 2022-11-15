1f7337da67511e3cca00f9f5d2bb544dc8e9eb1b
sooryap91 <sooryap1991@gmail.com>
2022-11-15 19:15:15 +0530

    changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = new express();
 const path = require('path');
-app.use(express.static('./dist/meanstackproject'));
+// app.use(express.static('./dist/meanstackproject'));
     
+app.use(express.static(path.join(__dirname+'/dist/meanstackproject')));
 
 
 
